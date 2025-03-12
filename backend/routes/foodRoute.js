5d52bd941ec9f02c5329cecd3895a9292a5abaf9
Najmun Nahar Hira <107489391+najmunnaharhira@users.noreply.github.com>
2025-03-12 23:34:31 +0600

    foodController.js modified done

--- backend/routes/foodRoute.js
+++ backend/routes/foodRoute.js
@@ -1,24 +1,21 @@
-import express  from "express";
-
-import { addFood } from "../controllers/foodControllers.js";
-
+import express from "express";
 import multer from "multer";
+import { addFood } from "../controllers/foodControllers.js";
+import { addFood, listFood } from "../controllers/foodControllers.js";
 
 const foodRouter = express.Router();
 
 //image Storeage Engine
 
 const storage = multer.diskStorage({
-  destination:"uploads",
-  filename:(req,file,cb)=>{
-    return cb(null,`${Date.now()}${file.originalname}`)
-  }
-})
-const upload = multer({storage:storage})
- 
-foodRouter.post("/add",upload.single("image"),addFood)
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    return cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
+const upload = multer({ storage: storage });
 
-
-
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.get("/list", listFood);
 
 export default foodRouter;
--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,26 @@
-import express from "express"
-import cors from "cors"
-import { connectDB } from "./config/db.js"
-import foodRouter from "./routes/foodRoute.js"
+import cors from "cors";
+import express from "express";
+import foodRouter from "./routes/foodRoute.js";
+import { connectDB } from "./config/db.js";
 
-const app=express()
+const app = express();
 
-const port=4000
+const port = 4000;
 
-
-app.use(express.json())
-app.use(cors())
-//db connection 
+app.use(express.json());
+app.use(cors());
+//db connection
 connectDB();
-//api endpoints 
+//api endpoints
 
-app.use("/api/food",foodRouter)
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
 
+app.get("/", (req, res) => {
+  res.send("API working");
+});
 
-
-
-app.get("/",(req,res)=>{
-  res.send("API working")
-})
-
-app.listen(port,()=>{
-  console.log(`Server Started on http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server Started on http://localhost:${port}`);
+});
 // mongodb+srv://ibrahim036sust:aE54X59FtUwxUWij@cluster0.asgqfnl.mongodb.net/?
