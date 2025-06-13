7c9d8035b5d9fe45a7a51b037309bda44dbed67b
Najmun Nahar Hira <107489391+najmunnaharhira@users.noreply.github.com>
2025-06-13 19:25:32 +0600

    update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,44 +1,110 @@
-import React from 'react'
-import './Footer.css'
-import { assets } from '../../assets/assets'
-const Footer = () => {
-  return (
-    <div className='footer' id='footer'>
-      <div className='footer-content'>
-        <div className="footer-content-left">
-           <img src={assets.logo} alt=""/>
-           <p>lorem ipsum is simple dummy text ot the printing and typesetting industryl.Lorem is very good hori is very bad I am hungry for </p>
-           <div className="footer-social-icons">
-            <img src={assets.facebook_icon} alt="" />
-            <img src={assets.twitter_icon} alt="" />
-            <img src={assets.linkedin_icon} alt="" />
-           </div>
-        </div>
-        <div className="footer-content-center">
-           <h2>COMPANY</h2>
-           <ul>
-             <li>Home</li>
-             <li>About us</li>
-             <li>Delivery</li>
-             <li>Privacy policy</li>
-           </ul>
-        </div>
-        <div className="footer-content-right">
-            <h2>GET IN TOUCH</h2>
-            <ul>
-              <li>+01855849434</li>
-              <li>contact@tomato.com</li>
-            </ul>
-        </div>
-    
-      </div>
-      <hr/>
-      <p className="footer-copyright">
-        Copyright 2024 @ Tomato.com-All Right Reserved.
-      </p>
-      
-    </div>
-  )
-}
+import "./Footer.css";
+import React, { useContext, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { assets } from "../../assets/assets";
+import { StoreContext } from "../../context/StoreContext";
 
-export default Footer
+const Navbar = ({ setShowLogin }) => {
+  const { getTotalCartAmount, token, setToken } = useContext(StoreContext);
+  const [menu, setMenu] = useState("home");
+  const navigate = useNavigate();
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setToken("");
+    navigate("/");
+  };
+
+  return (
+    <div className="navbar">
+      <Link to="/">
+        <img src={assets.logo} alt="" className="logo" />
+      </Link>
+      <ul className="navbar-menu">
+        <Link
+          to="/"
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          home
+        </Link>
+        <a
+          href="#explore-menu"
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          menu
+        </a>
+        <a
+          href="#app-download"
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          mobile-app
+        </a>
+        <a
+          href="#footer"
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          contact us
+        </a>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <Link to="/cart">
+            <img src={assets.basket_icon} alt="" />
+          </Link>
+          <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
+        </div>
+        {!token ? (
+          <button onClick={() => setShowLogin(true)}>sign in</button>
+        ) : (
+          <div className="navbar-profile">
+            <img src={assets.profile_icon} alt=" " />
+            <ul className="nav-profile-dropdown">
+              <li onClick={() => navigate("/myorders")}>
+                <img src={assets.bag_icon} alt=" " />
+                <p>Orders</p>
+              </li>
+              <hr />
+              <li onClick={logout}>
+                <img src={assets.logout_icon} alt=" " />
+                <p>Logout</p>
+              </li>
+            </ul>
+          </div>
+        )}
+        <div className="social-links">
+          <a
+            href="https://www.facebook.com/najmunnaharhira360"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-icon"
+          >
+            <img src={assets.facebook_icon} alt="Facebook" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/najmunnaharhira360"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-icon"
+          >
+            <img src={assets.linkedin_icon} alt="LinkedIn" />
+          </a>
+          <a
+            href="https://twitter.com/najmunnaharhira360"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="social-icon"
+          >
+            <img src={assets.twitter_icon} alt="Twitter" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
