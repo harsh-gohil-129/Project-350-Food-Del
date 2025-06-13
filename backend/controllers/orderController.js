04c9385148cc01bb29c5b9bec018bbde52194dd4
Najmun Nahar Hira <107489391+najmunnaharhira@users.noreply.github.com>
2025-06-13 17:52:26 +0600

    Full Project Finished Successfully

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -105,4 +105,28 @@ const updaateStatus = async (req, res) => {
     
 }
 
+//Listing orders  for admin Panel
+
+const listOrders = async (req, res) => {
+    try {
+        const orders = await orderModel.find({});
+        res.json({success:true,data:orders})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+//api for updating order status
+
+const updateStatus = async (req, res) => {
+    try
+    {
+        await orderModel.findByIdAndUpdate(req.body.orderId, { status: req.body.status })
+        res.json({success:true,message:"Status Updated"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+
 export { placeOrder,verifyOrder,userOrders };
