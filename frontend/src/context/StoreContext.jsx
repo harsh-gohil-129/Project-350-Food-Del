88dc946e4688531476d884ef50ee18ab3e0b0402
Najmun Nahar Hira <107489391+najmunnaharhira@users.noreply.github.com>
2025-06-13 19:01:56 +0600

    Navbar.jssx edited

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,6 +1,7 @@
-import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 import { food_list as foodData } from "../assets/assets";
+
+// import axios from "axios";
 
 // import axios from "axios";
 // import { createContext, useEffect, useState } from "react";
--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,8 +1,9 @@
 import "./PlaceOrder.css";
 import React, { useContext, useEffect, useState } from "react";
-import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { StoreContext } from "../../context/StoreContext";
+
+// import axios from "axios";
 
 // import "./PlaceOrder.css";
 // import React, { useContext, useState } from "react";
